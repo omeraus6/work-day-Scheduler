beb95895ded0a294d59dfeb0d94872753f2a5b6e
omeraus6 <omeraus6@yahoo.com>
2023-11-06 22:53:51 -0800

    Add the events color and save message

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # work-day-Scheduler
+
+## Description
+
+- What was your motivation? I learn alot about Web-APIs , third-party-APIs and jquery for javascript 
+- Why did you build this project? I build this project to make schedular for work and can help employee ,students or personal used.
+1- I add a day time for work day scheduler
+2- when click save button can save the event at the current time location and message show for data save 
+3- add line between the header and body
+4- events color change , present , current and past.
+5- used localstorge to save event information 
+- What did you learn? I learn web-APIs, third-party-APIs and jquery for javascript and how to used the time and date.
+
+## Installation
+
+N/A
+
+## Usage
+when open the webpage, it will show the work day scheduler and the date then under it , you will see the time from 9AM to 5PM, every hour has save button and area to add commit for the event, when click save it will save it.
+there are some colors in the event status, green color means no event , gray color means the event was due and red color means has event setup.
+
+https://omeraus6.github.io/work-day-Scheduler/
+deploy link
+
+https://github.com/omeraus6/work-day-Scheduler
+github link
+
+
+
+![alt text](assets/images/1.png)
+image 1: 
+
+![alt text](assets/images/2.png)
+image 2: the first 2 image are the code
+
+![alt text](assets/images/3.png)
+image 3:  the webpage show the Date and the event color , current ,past and future
+
+![alt text](assets/images/4.png)
+image 4: show the save message 
+
+
+## Credits
+
+Thank you for tips and sugestion from Bootcamp instructors and classmates, I hava used https://www.w3schools.com/ , https://stackoverflow.com/ and https://developer.mozilla.org/en-US/docs/Web/CSS/grid-template to reserch information 
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,57 @@
-var reformatDate = dayjs().format('dddd, MMMM D');
-$('#currentDay').text(reformatDate);
-
-//var advancedFormat = require('dayjs/plugin/advancedFormat');
-//dayjs().extend(advancedFormat);
-
-//var reformatDate = dayjs('2020-11-03').format('dddd, MMMM D YYYY, h:mm:ss a');
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  
-  //alert(dayjs().format("hhA"));
-  //alert(parseInt(dayjs().format("hh")));
+
+   //add date for today
+  var reformatDate = dayjs().format('dddd, MMMM D');
+  $('#currentDay').text(reformatDate);
+
+  //return the event from localstorage and show them in sheduler
   for(var i=1;i<=12;i++)
   {
 
     var data= $.parseJSON(localStorage.getItem("hour-"+i));
+    $('#hour-'+i).css("background-color", "#77dd77");
     
     if(data !== null && data != "")
     {
       
       $('#hour-'+i).find('textarea').val($.parseJSON(localStorage.getItem("hour-"+i)));
-      $('#hour-'+i).css("background-color", "#ff6961");
-      
-      if(parseInt(dayjs().format("hh")) <= i)
+
+      if(parseInt(dayjs().format("hh")) > i)
       {
         $('#hour-'+i).css("background-color", "#d3d3d3");
       }
-      
-    }
-    else if(data == "" && data !== null)
-    {
-      $('#hour-'+i).css("background-color", "#77dd77");
+      else if(parseInt(dayjs().format("hh")) == i)
+      {
+        $('#hour-'+i).css("background-color", "#ff6961");
+      }
     }
     else
     {
       $('#hour-'+i).css("background-color", "#77dd77");
     }
-
     
   }
 
+
+  // save button "addEventListener"
   $('button').on('click', function (event) {
     event.preventDefault();
     var attr= this.id;
-    //var divattr= $('<div>').id('hour-9');
 
     for(var i=1;i<=12;i++)
     {
-      //if(i<=5 || i>=9)
-      //{
         if(attr == "hour"+i)
         {
-          alert('helloooo: ' + attr);
-          //$('#hour-'+i).find('textarea').val(localStorage.setItem(attr,JSON.stringify(storedata)));
           var storedata = $('#hour-'+i).find('textarea').val();
           localStorage.setItem('hour-'+i,JSON.stringify(storedata));
+          $('#savemsg').text("Appoiment add to localstorage");
+          $('#savemsg').css('color' , '#dc3545');
           
         }
     }
-
-
-        
-        //JSON.parse(localStorage.getItem("index"+index));
-        //para.textContent = JSON.parse(localStorage.getItem("index"+ i)) ;
-        //localStorage.setItem(attr,JSON.stringify(storedata));
-        //alert($('<div>').id =);
-      //}
-     // if(attr == "hour9")
-      //{
-        //alert("heloooo button");
-      //}
-
-      //$("<div>").each(function(){
-        //if($(this).attr("id")== attr){
-         //alert('helooooooo div');
-        //}
-       //});
-
-  //}
-  
-  
-
-
 
 
     // TODO: Add a listener for click events on the save button. This code should
